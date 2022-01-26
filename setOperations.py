9594a56277abc60a630f9ed32c9bfa7c5da84b31
Parag <paragbokde51@gmail.com>
2022-01-26 19:53:55 +0530

    Pushed setOperations

---
+++ setOperations.py
@@ -0,0 +1,7 @@
+set1 = set([0, 2, 4, 6, 8])
+set2 = set([1, 2, 3, 4, 5])
+
+print("Union: " + str(set1 | set2))
+print("Intersection: " + str(set1 & set2))
+print("Union: " + str(set1 - set2))
+print("Union: " + str(set1 ^ set2))
